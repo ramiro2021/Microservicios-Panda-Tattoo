ad437e4ea8ec5f1ac5dcb70e372e1c3548973242
ramiro2021 <ramiroamurua@hotmail.com>
2020-12-31 16:39:49 -0300

    feat: crud tatuadores + relacion trabajo

--- microservicios-tatuadores/src/main/java/com/ram/microservicios/app/tatuadores/models/entity/Tatuador.java
+++ microservicios-tatuadores/src/main/java/com/ram/microservicios/app/tatuadores/models/entity/Tatuador.java
@@ -1,16 +1,23 @@
 package com.ram.microservicios.app.tatuadores.models.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "Tatuadores")
@@ -29,11 +36,37 @@ public class Tatuador {
 	private Date createAt;
 	
 	
+	@JsonIgnoreProperties(value = {"tatuador"}, allowSetters = true)
+	@OneToMany(mappedBy = "tatuador", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Trabajo> trabajos;
+	
+	
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
 	}
 	
+		
+	public Tatuador() {
+		this.trabajos = new ArrayList<>();
+	}
+
+
+	public List<Trabajo> getTrabajos() {
+		return trabajos;
+	}
+
+
+
+
+	public void setTrabajos(List<Trabajo> trabajos) {
+		this.trabajos.clear();
+		trabajos.forEach(this::addTrabajos);
+	}
+
+
+
+
 	public Long getId() {
 		return id;
 	}
@@ -66,6 +99,34 @@ public class Tatuador {
 		this.createAt = createAt;
 	}
 	
+	
+	
+	public void addTrabajos(Trabajo trabajo) {
+		this.trabajos.add(trabajo);
+		trabajo.setTatuador(this);
+	}
+	
+	public void removeTrabajos(Trabajo trabajo) {
+		this.trabajos.remove(trabajo);
+		trabajo.setTatuador(null);
+	}
+	
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		
+		if (!(obj instanceof Tatuador)) {
+			return false;
+		}
+		
+		Tatuador tatuador = (Tatuador) obj;
+		
+		return this.id != null && this.id.equals(tatuador.getId());
+	}
 	
 	
 }
---
+++ microservicios-tatuadores/src/main/java/com/ram/microservicios/app/tatuadores/models/entity/Trabajo.java
@@ -0,0 +1,87 @@
+package com.ram.microservicios.app.tatuadores.models.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "trabajos")
+public class Trabajo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String titulo;
+	private String Descripcion;
+
+	@Column(name = "create_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createAt;
+	
+	@JsonIgnoreProperties(value = {"trabajo"})
+	@ManyToOne
+	@JoinColumn(name = "tatuador_id")
+	private Tatuador tatuador;
+
+	@PrePersist
+	public void prePersist() {
+		this.createAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescripcion() {
+		return Descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public Tatuador getTatuador() {
+		return tatuador;
+	}
+
+	public void setTatuador(Tatuador tatuador) {
+		this.tatuador = tatuador;
+	}
+	
+	
+
+}
